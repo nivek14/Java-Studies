84dd8d345cb4e8d424abea0c99a08d99ca0d9e31
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-10-13 14:25:30 -0300

    feat: Adicionando getters e setter
    - add getters e setters
    - add lombok no pom.xml

--- Rockseat - Aplicação backend/todolist/todolist/src/main/java/br/com/ApplicationStudy/todolist/user/UserController.java
+++ Rockseat - Aplicação backend/todolist/todolist/src/main/java/br/com/ApplicationStudy/todolist/user/UserController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// uma controller irá processar as requisições http e atuar como a primeira camada de acesso do usuário a aplicação
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
+    // usando o RequestBody, por baixo dos panos ele já irá setar os dados em um objeto do tipo UserModel
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public void create(@RequestBody UserModel userModel){;
+        System.out.println(userModel.getName());
     }
 
 }
--- Rockseat - Aplicação backend/todolist/todolist/src/main/java/br/com/ApplicationStudy/todolist/user/UserModel.java
+++ Rockseat - Aplicação backend/todolist/todolist/src/main/java/br/com/ApplicationStudy/todolist/user/UserModel.java
@@ -1,5 +1,14 @@
 package br.com.ApplicationStudy.todolist.user;
 
+import lombok.Data;
+
+/**
+ * @Getter: Irá apenas adicionar os getters
+ * @Setter: Irá apenas adicionar os setters
+ * @Data: Irá adcionar os getters e os setters
+ * Biblioteca: Lombok
+ */
+@Data
 public class UserModel {
 
     private String username;
