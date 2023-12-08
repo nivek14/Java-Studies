727ca0610b4ceba55226488b9d7e8dfd83751505
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-08 00:49:13 -0300

    leetcode problem 67 solved

---
+++ LeetCode/LeetCode/src/solutions/LC67.java
@@ -0,0 +1,18 @@
+package solutions;
+
+import java.math.BigInteger;
+
+public class LC67 {
+
+    public String addBinary(String a, String b) {
+
+        BigInteger aBinary = new BigInteger(a,2);
+        BigInteger bBinary = new BigInteger(b,2);
+
+        BigInteger sum = aBinary.add(bBinary);
+
+        return sum.toString(2);
+
+    }
+
+}
