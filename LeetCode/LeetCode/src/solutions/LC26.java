33ac8f79d3236def9fb3da2366776f498c67b3da
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-14 00:34:44 -0300

    Create LC26.java
    falta finalizar

---
+++ LeetCode/LeetCode/src/solutions/LC26.java
@@ -0,0 +1,27 @@
+package solutions;
+
+public class LC26 {
+    // finalizar
+    public int removeDuplicates(int[] nums) {
+
+        int k;
+        int [] newNums = new int[nums.length];
+        int dinamycInex = 0;
+        boolean flag = false;
+
+        for(int i = dinamycInex; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                dinamycInex = j;
+                if(nums[i] == nums[j] && flag == false){
+                    newNums[i] = nums[i];
+                    flag = true;
+                }
+            }
+            flag = false;
+        }
+
+        return newNums.length;
+
+    }
+
+}
