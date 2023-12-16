1da0d04deee0c4a6f273767a9120ecf9c8c120c0
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-15 23:09:17 -0300

    add LC1929

--- LeetCode/LeetCode/src/Main.java
+++ LeetCode/LeetCode/src/Main.java
@@ -1,19 +1,21 @@
 import solutions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         // Instâncias dos problemas
-        LC1716 test1716 = new LC1716();
-        LC1 test1 = new LC1();
-        LC9 test9 = new LC9();
-        LC67 test67 = new LC67();
-        LC169 test169 = new LC169();
-        LC1436 test1436 = new LC1436();
-        LC191 test191 = new LC191();
+        LC1716  test1716    = new LC1716();
+        LC1     test1       = new LC1();
+        LC9     test9       = new LC9();
+        LC67    test67      = new LC67();
+        LC169   test169     = new LC169();
+        LC1436  test1436    = new LC1436();
+        LC191   test191     = new LC191();
+        LC1929  test1919    = new LC1929();
 
         // Entradas personalizadas
         ArrayList<List<String>> input1436 = new ArrayList<List<String>>();
@@ -28,6 +30,7 @@ public class Main {
         //System.out.println(test67.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
         //System.out.println(test169.majorityElement(new int[]{2,2,1,1,1,2,2}));
         //System.out.println(test1436.destCity(input1436));
-        System.out.println(test191.hammingWeight(00000000000000000000000000001011 ));
+        //System.out.println(test191.hammingWeight(00000000000000000000000000001011 ));
+        System.out.println(Arrays.toString(test1919.getConcatenation(new int[]{1,3,2,1})));
     }
 }
---
+++ LeetCode/LeetCode/src/solutions/LC1929.java
@@ -0,0 +1,20 @@
+package solutions;
+
+public class LC1929 {
+
+    public int[] getConcatenation(int[] nums) {
+
+        int [] concArray = new int[nums.length * 2];
+        int j = 0;
+
+        for(int i = 0; i < concArray.length; i++){
+            if(j >= nums.length) j = 0;
+            concArray[i] = nums[j];
+            j++;
+        }
+
+        return concArray;
+
+    }
+
+}
