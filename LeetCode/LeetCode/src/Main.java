24ddd4ed47f551452889d41bc164fa9137711be1
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-15 21:49:31 -0300

    add LC191

--- LeetCode/LeetCode/src/Main.java
+++ LeetCode/LeetCode/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         LC67 test67 = new LC67();
         LC169 test169 = new LC169();
         LC1436 test1436 = new LC1436();
+        LC191 test191 = new LC191();
 
         // Entradas personalizadas
         ArrayList<List<String>> input1436 = new ArrayList<List<String>>();
@@ -26,6 +27,7 @@ public class Main {
         //System.out.println(test9.isPalindrome(-121));
         //System.out.println(test67.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
         //System.out.println(test169.majorityElement(new int[]{2,2,1,1,1,2,2}));
-        System.out.println(test1436.destCity(input1436));
+        //System.out.println(test1436.destCity(input1436));
+        System.out.println(test191.hammingWeight(00000000000000000000000000001011 ));
     }
 }
---
+++ LeetCode/LeetCode/src/solutions/LC191.java
@@ -0,0 +1,22 @@
+package solutions;
+
+public class LC191 {
+
+    public int hammingWeight(int n) {
+
+        /*long value = n;
+        int countOnes = 0;
+        String convertedValue = Long.toBinaryString(value);
+
+        for(int i = 0; i < convertedValue.length(); i++){
+            if(convertedValue.charAt(i) == '1') countOnes++;
+        }
+
+        return countOnes;*/
+
+        return Integer.bitCount(n);
+        
+    }
+
+
+}
