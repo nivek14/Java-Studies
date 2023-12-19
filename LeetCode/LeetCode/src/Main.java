0b69e6f33b8e631c670b679e391e181ca15a523c
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-19 01:22:35 -0300

    add LC2535

--- LeetCode/LeetCode/src/Main.java
+++ LeetCode/LeetCode/src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         LC1630 test1630     = new LC1630(1,1,0);
         LC1672 test1672     = new LC1672();
         LC2798 test2798     = new LC2798();
+        LC2535 test2535     = new LC2535();
 
         // Entradas personalizadas
         ArrayList<List<String>> input1436 = new ArrayList<List<String>>();
@@ -55,6 +56,7 @@ public class Main {
         //System.out.println(Arrays.toString(test1470.shuffle(new int[]{1,1,2,2}, 2)));
         //System.out.println(Arrays.toString(new List[]{test2942.findWordsContaining(new String[]{"abc", "bcd", "aaaa", "cbc"}, 'a')}));
         //System.out.println(test1672.maximumWealth(new int[][]{{1,5},{7,3},{3,5}}));
-        System.out.println(test2798.numberOfEmployeesWhoMetTarget(new int[]{0,1,2,3,4}, 2));
+        //System.out.println(test2798.numberOfEmployeesWhoMetTarget(new int[]{0,1,2,3,4}, 2));
+        System.out.println(test2535.sum(1,2));
     }
 }
