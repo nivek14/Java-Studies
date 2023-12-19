9fefd5bb601e55372b68ef8afb52e5f60f27f4fc
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2023-12-19 00:44:04 -0300

    add LC1630

---
+++ LeetCode/LeetCode/src/solutions/LC1630.java
@@ -0,0 +1,32 @@
+package solutions;
+
+public class LC1630 {
+
+    private int big, medium, small;
+
+    public LC1630(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+
+        if(carType == 1 && big != 0){
+            big--;
+            return true;
+        }
+        else if(carType == 2 && medium != 0){
+            medium--;
+            return true;
+        }
+        else if(carType == 3 && small != 0){
+            small--;
+            return true;
+        }
+
+        else return false;
+
+    }
+
+}
