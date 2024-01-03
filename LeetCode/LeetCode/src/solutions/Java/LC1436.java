68c6db92e47729fffc2ff27b03b380a34266a189
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-01-03 17:25:30 -0300

    refactor na estrutura dos arquivos

--- LeetCode/LeetCode/src/solutions/Java/LC1436.java
+++ LeetCode/LeetCode/src/solutions/Java/LC1436.java
@@ -1,4 +1,4 @@
-package solutions;
+package solutions.Java;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- LeetCode/LeetCode/src/solutions/Java/LC67.java
+++ LeetCode/LeetCode/src/solutions/Java/LC67.java
@@ -1,4 +1,4 @@
-package solutions;
+package solutions.Java;
 
 import java.math.BigInteger;
 
