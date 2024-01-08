714178b4f6240a30a73452e247c893b2aac98a79
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-01-08 14:31:06 -0300

    atualização no controller da calculadora

--- demo/demo/src/main/java/com/example/demo/controllers/CalculadoraController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/CalculadoraController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.*;
 public class CalculadoraController {
 
     @GetMapping("/{a}/{b}")
+    // na url fica: http://localhost:8080/calculadora/10/5
     public int soma(@PathVariable int a, @PathVariable int b){
         Calculadora calc = new Calculadora(a, b);
         return calc.getA() + calc.getB();
     }
 
-    @GetMapping
+    @GetMapping(path = "subtrair")
+    // na url fica: http://localhost:8080/calculadora/subtrair?a=10&b=5
     public int subtrair(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
         Calculadora calc = new Calculadora(a, b);
         return calc.getA() - calc.getB();
