5fa5f8e11493df0613b1bfa3caadb6b735a6441f
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-01-29 16:54:47 -0300

    crud: adição do controller e entidade de produtos e mais algumas configurações

---
+++ CRUD/demo/src/main/java/com/example/demo/domain/product/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.domain.product;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    private Number price_in_cents;
+
+}
