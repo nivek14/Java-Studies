bab40e01b1d954c64d5a98157a7f9f74abf31e73
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-02-05 21:00:04 -0300

    SoftBlues: Adicionando estrutura básica do sistema

---
+++ SoftBlues System/SoftBlues/src/main/java/com/example/SoftBlues/SoftBluesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoftBlues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftBluesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftBluesApplication.class, args);
+	}
+
+}
---
+++ SoftBlues System/SoftBlues/src/test/java/com/example/SoftBlues/SoftBluesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftBlues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftBluesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
